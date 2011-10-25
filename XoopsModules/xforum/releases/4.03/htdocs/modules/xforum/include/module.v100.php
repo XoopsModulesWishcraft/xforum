1c7061a2e82af578dc8f4f78ff5a3966b06d8d96
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-10-25 11:13:33 +0000

    Changing TYPE=MyISAM to ENGINE=MyISAM

--- XoopsModules/xforum/releases/4.03/htdocs/modules/xforum/include/module.v100.php
+++ XoopsModules/xforum/releases/4.03/htdocs/modules/xforum/include/module.v100.php
@@ -35,7 +35,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		`topic_id` tinyint(8) NOT NULL default '0',
 		`post_id` tinyint(8) NOT NULL default '0',
 	  	`post_text` text NOT NULL
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_archive");
 	}
@@ -51,7 +51,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		`download` int(10) NOT NULL default '0',
 		PRIMARY KEY  (`attach_id`),
 		KEY `post_id` (`post_id`)
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_attachments");
 	}
@@ -62,7 +62,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		`digest_content` text,
 		PRIMARY KEY  (`digest_id`),
 		KEY `digest_time` (`digest_time`)
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_digest");
 	}
@@ -74,7 +74,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		`online_uname` varchar(255) default NULL,
 		`online_ip` varchar(32) default NULL,
 		`online_updated` int(14) default NULL
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_online");
 	}
@@ -90,7 +90,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		`report_memo` varchar(255) default NULL,
 		PRIMARY KEY  (`report_id`),
 		KEY `post_id` (`post_id`)
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_report");
 	}
@@ -106,7 +106,7 @@ function xoops_module_update_XFORUM_v100(&$module)
 		KEY ratinguser (ratinguser),
 		KEY ratinghostname (ratinghostname),
 		KEY topic_id (topic_id)
-		) TYPE=MyISAM");
+		) ENGINE=MyISAM");
 	if (!$result) {
 		$module->setErrors("Could not create xf_votedata");
 	}
